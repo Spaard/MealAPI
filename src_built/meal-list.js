33c74d71d38ac704e94e683f7356427e3c65df7a
Mathiscrack <mathisaac03@gmail.com>
2025-02-15 13:56:37 +0100

    cleaning

--- src_built/meal-list.js
+++ src_built/meal-list.js
@@ -13,51 +13,6 @@ import { LitElement, html, css } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import { Task } from '@lit/task';
 import './meal-element';
-/**const dishes : Dish[] = [
-    {
-        idMeal : "52771",
-        name : "Spicy Arrabiata Penne",
-        category : "Vegetarian",
-        instructions : "Bring a large pot of water to a boil. Add kosher salt to the boiling water, then add the pasta. Cook according to the package instructions, about 9 minutes.\r\nIn a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, 1 to 2 minutes. Add the chopped tomatoes, red chile flakes, Italian seasoning and salt and pepper to taste. Bring to a boil and cook for 5 minutes. Remove from the heat and add the chopped basil.\r\nDrain the pasta and add it to the sauce. Garnish with Parmigiano-Reggiano flakes and more basil and serve warm.",
-        urlImage : "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg"
-    },{
-        idMeal : "52772",
-        name : "Tortilla de patata",
-        category : "Vegetarian",
-        instructions : "Test of recipe",
-        urlImage : "https://tse1.mm.bing.net/th?id=OIP.mA5jkJjX5oonrhLLVEsWfwHaE-&pid=Api"
-    },{
-        idMeal : "52771",
-        name : "Spicy Arrabiata Penne",
-        category : "Vegetarian",
-        instructions : "Bring a large pot of water to a boil. Add kosher salt to the boiling water, then add the pasta. Cook according to the package instructions, about 9 minutes.\r\nIn a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, 1 to 2 minutes. Add the chopped tomatoes, red chile flakes, Italian seasoning and salt and pepper to taste. Bring to a boil and cook for 5 minutes. Remove from the heat and add the chopped basil.\r\nDrain the pasta and add it to the sauce. Garnish with Parmigiano-Reggiano flakes and more basil and serve warm.",
-        urlImage : "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg"
-    },{
-        idMeal : "52771",
-        name : "Spicy Arrabiata Penne",
-        category : "Vegetarian",
-        instructions : "Bring a large pot of water to a boil. Add kosher salt to the boiling water, then add the pasta. Cook according to the package instructions, about 9 minutes.\r\nIn a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, 1 to 2 minutes. Add the chopped tomatoes, red chile flakes, Italian seasoning and salt and pepper to taste. Bring to a boil and cook for 5 minutes. Remove from the heat and add the chopped basil.\r\nDrain the pasta and add it to the sauce. Garnish with Parmigiano-Reggiano flakes and more basil and serve warm.",
-        urlImage : "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg"
-    },{
-        idMeal : "52771",
-        name : "Spicy Arrabiata Penne",
-        category : "Vegetarian",
-        instructions : "Bring a large pot of water to a boil. Add kosher salt to the boiling water, then add the pasta. Cook according to the package instructions, about 9 minutes.\r\nIn a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, 1 to 2 minutes. Add the chopped tomatoes, red chile flakes, Italian seasoning and salt and pepper to taste. Bring to a boil and cook for 5 minutes. Remove from the heat and add the chopped basil.\r\nDrain the pasta and add it to the sauce. Garnish with Parmigiano-Reggiano flakes and more basil and serve warm.",
-        urlImage : "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg"
-    },{
-        idMeal : "52771",
-        name : "Spicy Arrabiata Penne",
-        category : "Vegetarian",
-        instructions : "Bring a large pot of water to a boil. Add kosher salt to the boiling water, then add the pasta. Cook according to the package instructions, about 9 minutes.\r\nIn a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, 1 to 2 minutes. Add the chopped tomatoes, red chile flakes, Italian seasoning and salt and pepper to taste. Bring to a boil and cook for 5 minutes. Remove from the heat and add the chopped basil.\r\nDrain the pasta and add it to the sauce. Garnish with Parmigiano-Reggiano flakes and more basil and serve warm.",
-        urlImage : "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg"
-    },{
-        idMeal : "52771",
-        name : "Spicy Arrabiata Penne",
-        category : "Vegetarian",
-        instructions : "Bring a large pot of water to a boil. Add kosher salt to the boiling water, then add the pasta. Cook according to the package instructions, about 9 minutes.\r\nIn a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, 1 to 2 minutes. Add the chopped tomatoes, red chile flakes, Italian seasoning and salt and pepper to taste. Bring to a boil and cook for 5 minutes. Remove from the heat and add the chopped basil.\r\nDrain the pasta and add it to the sauce. Garnish with Parmigiano-Reggiano flakes and more basil and serve warm.",
-        urlImage : "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg"
-    }
-]*/
 let MealListElement = class MealListElement extends LitElement {
     constructor() {
         super(...arguments);
